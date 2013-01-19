e370a7311fb1981e793ef7206bc6ddb9e0cf6f67
Cody P Schafer <jmesmon@gmail.com>
2013-01-19 09:43:04 -0500

    start building the server component.

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ all::
 obj-libsane-bro2.so = brother2.o tcp.o sane_strstatus.o
 libsane-bro2.so : LDFLAGS+= -shared
 libsane-bro2.so : CFLAGS+= -fpic
-TARGETS = libsane-bro2.so
+
+obj-bro2-serv = brother2-serv.o tcp.o
+
+TARGETS = libsane-bro2.so bro2-serv
 
 include base.mk
--- brother2-serv.c
+++ brother2-serv.c
@@ -1,8 +1,16 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+
 #include "tcp.h"
+#include "bro2.h"
 
 int main(int argc, char **argv)
 {
-	char *bind_addr = NULL, *port = BRO2_PORT_STR;
+	const char *bind_addr = NULL, *port = BRO2_PORT_STR;
 	int opt;
 
 	while ((opt = getopt(argc, argv, "a:p:")) != -1) {
@@ -23,8 +31,16 @@ int main(int argc, char **argv)
 	struct addrinfo *res;
 	int r = tcp_resolve_listen(bind_addr, port, &res);
 	if (r) {
-		fprintf("could not resolve %s: %s\n", bind_addr, gai_strerror(r));
-		return 0;
+		fprintf(stderr, "could not resolve %s: %s\n", bind_addr, gai_strerror(r));
+		return 1;
 	}
 
+	int fd = tcp_bind(res);
+	if (fd == -1) {
+		fprintf(stderr, "could not bind to addr %s, port %s: %s\n", bind_addr, port, strerror(errno));
+		return 1;
+	}
+
+	/* TODO: accept loop */
+	return 0;
 }
