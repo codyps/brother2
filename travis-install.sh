4f77d4d0f10ee30ed65ffeb4dce0f6746f2dd2cc
Cody P Schafer <devel@codyps.com>
2013-11-02 15:36:34 -0400

    travis?

--- travis-install.sh
+++ travis-install.sh
@@ -6,12 +6,18 @@ sudo apt-get install libsane-dev
 ## Ubuntu has 5.4.3, we need at v5.5+ release for
 ## netsnmp_indexed_addr_pair.
 
+
 PN=net-snmp
 PV=5.7.2
 P=$PN-$PV
 
+mkdir -p /tmp/$P &&
+cd /tmp/$P &&
+
 wget http://sourceforge.net/projects/net-snmp/files/$PN/$PV/${P}.tar.gz &&
 tar xf ${P}.tar.gz &&
 cd ${P} &&
-./configure && make && sudo make install
+
+./configure  --disable-agent --disable-embedded-perl --disable-mibs --disable-applications --disable-manuals --disable-scripts --with-defaults &&
+make && sudo make install
 
