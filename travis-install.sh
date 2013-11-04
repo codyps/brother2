1e1d5912a0061db69f2766d36f57b354f404f554
Cody P Schafer <devel@codyps.com>
2013-11-03 20:33:53 -0500

    travis-ci clang fixup?

--- travis-install.sh
+++ travis-install.sh
@@ -4,8 +4,10 @@
 #/usr/bin/ld: /usr/local/clang/bin/../lib/LLVMgold.so: error loading plugin
 #/usr/bin/ld: /usr/local/clang/bin/../lib/LLVMgold.so: error in plugin cleanup (ignored)
 #clang: error: linker command failed with exit code 1 (use -v to see invocation)
+sudo add-apt-repository ppa:xorg-edgers/ppa
 
-sudo apt-get install libsane-dev libev-dev
+sudo apt-get update
+sudo apt-get install libsane-dev libev-dev clang-3.3
 
 ## We need a newer version of net-snmp than ubuntu 12.04 has.
 ## Ubuntu has 5.4.3, we need at v5.5+ release for
